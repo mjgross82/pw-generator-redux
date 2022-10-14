f3ec787eebaca405019c971521fb7f13f5ae55f0
Michael Gross <mjgross@pm.me>
2022-10-14 13:49:36 -0400

    Added y/n radio for uppercase letters and special characters. Added form submit button.

--- index.php
+++ index.php
@@ -34,7 +34,18 @@
                 <input type="radio" name="lower" id="lowerYes" value=1>
                 <label for="lowerYes">Yes</label> 
                 <input type="radio" name="lower" id="lowerNo" value=0>
-                <label for="lowerNo">No</label><br>
+                <label for="lowerNo">No</label><br><br>
+                Contain uppercase letters?
+                <input type="radio" name="upper" id="upperYes" value=1>
+                <label for="upperYes">Yes</label> 
+                <input type="radio" name="upper" id="upperNo" value=0>
+                <label for="upperNo">No</label><br><br>
+                Contain special characters?
+                <input type="radio" name="special" id="specialYes" value=1>
+                <label for="specialYes">Yes</label> 
+                <input type="radio" name="special" id="specialNo" value=0>
+                <label for="specialNo">No</label><br><br>
+                <input type="submit" id="generate" class="btn" value="Generate Password">
             </form>
         </div>
       </div>

d18cba3fc8962885ce0590266d8e926758cf1b63
Michael Gross <mjgross@pm.me>
2022-10-14 15:42:43 -0400

    Added PHP to echo selection for lowercase letter radio button. Checked yes for all radio buttons by default.

--- index.php
+++ index.php
@@ -27,10 +27,25 @@
          <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $pwLength = $_POST['pwLength'];
+                $lower = $_POST['lower'];
+                if (is_null($lower) == true) {
+                    $lower = 2;
+                }
+                // $upper = $_POST['upper'];
+                // $special = $_POST['special'];
                 if (empty($pwLength)) {
-                    echo "Please enter a password length.";
+                    echo "Please enter a password length.\n";
                 } else {
-                    echo $pwLength;
+                    echo "Your password will be {$pwLength} characters long.\n";
+                };
+                if (isset($lower)) {
+                    if ($lower == "0") {
+                    echo "Password will not include lowercase letters.\n";
+                    } elseif ($lower == 1) {
+                    echo "Password will include lowercase letters.\n";
+                    } else {
+                    echo "Please make a selection for lowercase letters.\n";
+                    }
                 }
             }
             ?>
@@ -41,17 +56,17 @@
                 Password Length (Enter a number between 8 and 64):
                 <input type="number" name="pwLength" min="8" max="64"><br><br>
                 Contain lowercase letters?
-                <input type="radio" name="lower" id="lowerYes" value=1>
+                <input type="radio" name="lower" id="lowerYes" value=1 checked="checked">
                 <label for="lowerYes">Yes</label> 
                 <input type="radio" name="lower" id="lowerNo" value=0>
                 <label for="lowerNo">No</label><br><br>
                 Contain uppercase letters?
-                <input type="radio" name="upper" id="upperYes" value=1>
+                <input type="radio" name="upper" id="upperYes" value=1 checked="checked">
                 <label for="upperYes">Yes</label> 
                 <input type="radio" name="upper" id="upperNo" value=0>
                 <label for="upperNo">No</label><br><br>
                 Contain special characters?
-                <input type="radio" name="special" id="specialYes" value=1>
+                <input type="radio" name="special" id="specialYes" value=1 checked="checked">
                 <label for="specialYes">Yes</label> 
                 <input type="radio" name="special" id="specialNo" value=0>
                 <label for="specialNo">No</label><br><br>
